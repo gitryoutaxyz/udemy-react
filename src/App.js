034b034fd896a82d0cb1d2eb72fea431ba5beca8
Gitryoutaxyz <jazzallstar11@yahoo.co.jp>
2021-02-07 15:20:05 +0900

    create class component and function component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import react from 'react';
 import React,{ Component} from 'react';
 
+// クラスコンポーネント記述
 class App extends Component{
   render(){
     const greeting ='Hi'
@@ -9,6 +10,7 @@ class App extends Component{
     return dom;
   }
 }
+// // functionコンポーネント記述
 // function App() {
 //   const greeting ='Hi'
 //   // const dom = <React.Fragment><h1>{greeting}</h1><input type='text' onClick={()=>{console.log('im click')}}/></React.Fragment>;

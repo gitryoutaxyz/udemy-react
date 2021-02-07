7e2dcb7e9cf7fd5924683a930b6e7b19ab5bd601
Gitryoutaxyz <jazzallstar11@yahoo.co.jp>
2021-02-07 18:16:01 +0900

    learn state and setState

--- src/App.js
+++ src/App.js
@@ -1,54 +1,36 @@
-
-import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 
 // functionコンポーネント記述
-const App = () => {
-
-  const profiles = [
-    { name: 'taro', age: 15 },
-    { name: 'json', age: 18 },
-    { name:'ace',age:15 }
-  ];
-  const jobs =[
-    {name:"daiku"},
-    {name:"baseBall"}
-  ]
-  // return()内はjsxなのでjavascriptを記述するときは{}でくくる
-  return (
-    <div>
-      {profiles.map((profile,index) => {
-        // タグ<User />で関数を紐付 keyがないとエラー
-        return <User name={profile.name} age={profile.age} key={index}/>
-      })
-      }
-      {
-      jobs.map((job,index)=>{
-        return <Job name={job.name} key={index}/>
-      })
-      }
-    </div>
-  )
-}
-
-const User = (props) => {
-  return <div>I'am {props.name},{props.age}</div>
-}
-// デフォルト値を持たせる
-User.defaultProps = {
-  age:1
-}
-// 型
-User.propTypes ={
-  name:PropTypes.string.isRequired,
-  // isRequiredで必須としている
-  age:PropTypes.number
-}
+const App = () => (
+  // クラスコンポーネント
+  <Counter></Counter>
+);
 
 
-const Job =(props)=>{
-  return <div>
-    my job is {props.name}
-  </div>
-}
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    console.log(this.state);
+    this.state = { count: 0 };
+  }
+  countUp =()=> {
+    // console.log(current_state);
+    this.setState({count:this.state.count +1});
+  }
+  countDown=()=> {
+    // console.log(current_state);
+    this.setState({count:this.state.count -1});
 
+  }
+  render() {
+    console.log(this.state);
+    return (
+      <React.Fragment>
+        <div>counter:{this.state.count}</div>
+        <button onClick={this.countUp}>+</button>
+        <button onClick={this.countDown}>-</button>
+      </React.Fragment>
+    )
+  }
+}
 export default App;
